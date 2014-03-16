90681f370ec3e64f160bfabc04102e692f6028fa
NestorCenteno <nestor.centeno@nuvemtecnologia.mx>
2014-03-15 18:59:56 -0600

    fg

--- js/API/eventos.js
+++ js/API/eventos.js
@@ -1,7 +1,7 @@
 $(function(){
     document.addEventListener("deviceready",function(){
         obtenlista();
-        $("ul#ulp img.escuchar").live(function(){
+        $("ul#ulp").on('.escuchar','tap',function(){
             var ruta=$(this).attr('id');
             alert(ruta);
         });
